94c3a637ce9e04a711dedfb78d477a418f4fef0a
Admin <Admin@DESKTOP-19O37G6>
2021-12-21 15:16:47 +0530

    added both

---
+++ src/Threedigit_ArmstrongNumbers/DataTypesDemo.java
@@ -0,0 +1,29 @@
+package Threedigit_ArmstrongNumbers;
+
+public class DataTypesDemo {
+	
+
+		public static void main(String[] args)
+		{
+			//number(non decimal)
+			
+			byte num;	//1.variable declaration 
+			num=119;		//2.variable initialization 
+			System.out.println(num);//3.variable usage
+			
+			float avg=90.5535643646346f;
+			System.out.println(avg);
+			
+			char grade='B';
+			
+			System.out.println(grade);
+			
+			boolean result = true;
+			
+
+			System.out.println(result);
+		
+		}
+	}
+
+
---
+++ src/Threedigit_ArmstrongNumbers/else_if_condition.java
@@ -0,0 +1,36 @@
+package Threedigit_ArmstrongNumbers;
+
+
+	public class else_if_condition {
+
+		public static void main(String[] args) {
+			
+			int marks=66;
+			
+			
+			if (marks>=66.66)
+			{
+				System.out.println("Congratulation you got distinction");
+			}
+			
+			else if (marks>=50) 
+			{
+				System.out.println("You got 1st class");
+			}
+			
+			else if (marks>=35) 
+			{
+				System.out.println("You are passs");
+			}	
+			
+			
+			else  
+			{
+			System.out.println("Sorry you are failed");	
+			}
+
+		}
+
+	
+
+}
